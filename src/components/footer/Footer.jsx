40ea71369302839c71d86dfcc0f0c118f4d75e61
TaylorYT29 <taylorduransibaja@gmail.com>
2024-11-26 21:57:57 -0800

    img v2

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,7 @@
 import "../../index.css";
 import { FooterLink } from "./FooterLink";
 import { useTranslation } from "react-i18next";
+import logo from '../../../assets/imgs/logo-celeste.png';
 import { Link } from "react-router-dom";
 
 export function Footer(){
@@ -9,7 +10,7 @@ export function Footer(){
         <footer className="bg-black w-full grid lg:p-12 p-8 pt-5">
             <div className="grid gap-10 w-full grid-cols-[repeat(auto-fit,_minmax(200px,_1fr))] text-white">
                 <section className="max-w-[250px] text-justify">
-                    <img className="w-1/2" src="logo-celeste-C9fxKypw.png" alt="Logo" />
+                    <img className="w-1/2" src={logo} alt="Logo" />
                     <p className="text-white text-base mt-8">{t('footerEslogan')}</p>
                 </section>
 
